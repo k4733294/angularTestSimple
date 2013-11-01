3ffc0bcab2bab384dc11ced2a9c2ffb3e02d3bf8
hooshuu <k4733294@gmail.com>
2013-11-01 18:20:08 +0800

    rmitem

--- controllers.js
+++ controllers.js
@@ -1,9 +1,26 @@
-angular.module('todolistapp',[]);
+angular.module('todolistapp', []);
 
-function todocontroller($scope) {  
-  $scope.newItem = "";  
-  $scope.todoList = [{ label: "買牛奶" }, { label: "繳電話費" }];  
-  $scope.additem = function(){
-
-  }
+function todocontroller($scope) {
+	//$scope.newItem = "";  
+	$scope.todoList = [{
+		label: "買牛奶"
+	}, {
+		label: "繳電話費"
+	}];
+	$scope.additem = function() {
+		$scope.todoList.push($scope.newItem)
+	}
+}
+function TodoCrtlRemovable($scope) {  
+  $scope.newItem = '';  
+  $scope.todoList = [];  
+  $scope.addItem = function(){  
+    if(this.newItem){  
+       this.todoList.push({label:this.newItem,isFinish:false});  
+       this.newItem = '';  
+    }  
+  }  
+  $scope.removeItem = function(item){  
+      item.isFinish = true;  
+  }  
 }  
